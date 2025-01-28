e2299d5a9b694a0964b891808d3063c1d85a7687
chitra1985 <101934307+chitra1985@users.noreply.github.com>
2025-01-28 16:00:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('This is Machine Learning App')
 
 st.write('This app is Machine Learning app to predict stock price')
+
+with st.expander('Dataset'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://github.com/chitra1985/ML-Flow---Streamlit/blob/master/FAANG%20.csv')
